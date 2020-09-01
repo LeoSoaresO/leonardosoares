0caf9afd4ce420b2c6a8632d0ce8e8055a235bbb
LeoSoaresO <leonardo.rock22@hotmail.com>
2020-09-01 01:34:38 -0300

    Pagina de portfolio e slides

--- js/main.js
+++ js/main.js
@@ -14,3 +14,10 @@ new SimpleSlide({
     time: 10000, // tempo de transição dos slides
     pauseOnHover: true, // pausa a transição automática
   });
+new SimpleSlide ({
+    slide: 'portfolio', // nome do atributo data-slide="principal"
+    nav: true, // se deve ou não mostrar a navegação
+    auto: true, // se o slide deve passar automaticamente
+    time: 10000, // tempo de transição dos slides
+    pauseOnHover: true, // pausa a transição automática
+})
