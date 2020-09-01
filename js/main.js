7b9fd443f7db871ff49574787b20a79aae60775e
LeoSoaresO <leonardo.rock22@hotmail.com>
2020-08-31 23:08:02 -0300

    pagina de certificados com slides

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-new SimpleAnime()
+new SimpleAnime();
+
+new SimpleSlide({
+    slide: 'certificados', // nome do atributo data-slide="principal"
+    nav: false, // se deve ou não mostrar a navegação
+    auto: true, // se o slide deve passar automaticamente
+    time: 10000, // tempo de transição dos slides
+    pauseOnHover: true, // pausa a transição automática
+  });
+  new SimpleSlide({
+    slide: 'certificados-big', // nome do atributo data-slide="principal"
+    nav: false, // se deve ou não mostrar a navegação
+    auto: true, // se o slide deve passar automaticamente
+    time: 10000, // tempo de transição dos slides
+    pauseOnHover: true, // pausa a transição automática
+  });
