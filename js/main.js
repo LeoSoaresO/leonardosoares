3afdab1faeda66387f03dece932608de516d0060
LeoSoaresO <leonardo.rock22@hotmail.com>
2020-09-01 20:52:17 -0300

    SEO e Head

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
 new SimpleAnime();
 
 new SimpleSlide({
-    slide: 'certificados', // nome do atributo data-slide="principal"
-    nav: false, // se deve ou não mostrar a navegação
-    auto: true, // se o slide deve passar automaticamente
-    time: 10000, // tempo de transição dos slides
-    pauseOnHover: true, // pausa a transição automática
+    slide: 'certificados',
+    nav: false,
+    auto: true,
+    time: 10000,
+    pauseOnHover: true,
   });
   new SimpleSlide({
-    slide: 'certificados-big', // nome do atributo data-slide="principal"
-    nav: false, // se deve ou não mostrar a navegação
-    auto: true, // se o slide deve passar automaticamente
-    time: 10000, // tempo de transição dos slides
-    pauseOnHover: true, // pausa a transição automática
+    slide: 'certificados-big',
+    nav: false,
+    auto: true,
+    time: 10000,
+    pauseOnHover: true,
   });
 new SimpleSlide ({
-    slide: 'portfolio', // nome do atributo data-slide="principal"
-    nav: true, // se deve ou não mostrar a navegação
-    auto: true, // se o slide deve passar automaticamente
-    time: 10000, // tempo de transição dos slides
-    pauseOnHover: true, // pausa a transição automática
+    slide: 'portfolio',
+    nav: true,
+    auto: true,
+    time: 5000,
+    pauseOnHover: true,
 })
